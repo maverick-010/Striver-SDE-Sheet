03304c8e15b4ca5b4ec9aee495077537a7f6801e
Mohd Umar <44342762+maverick-010@users.noreply.github.com>
2022-06-04 07:27:29 -0700

    Create Maximum Subarray Sum.cpp

---
+++ Arrays/Hard/Maximum Subarray Sum.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h> 
+long long maxSubarraySum(int arr[], int n)
+{
+   long long sum=0;
+    long long ans=0;
+    for(int i=0;i<n;i++){
+        sum+=arr[i];
+        ans = max(sum,ans);
+        if(sum<0)
+            sum=0;
+    }
+    return ans;
+}
