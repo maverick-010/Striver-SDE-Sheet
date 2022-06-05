1406f240c007a2510b153e7c149d984f366c137c
Mohd Umar <44342762+maverick-010@users.noreply.github.com>
2022-06-05 13:14:31 -0700

    Create Check Permutation.cpp

---
+++ Strings/Easy/Check Permutation.cpp
@@ -0,0 +1,8 @@
+bool areAnagram(string &str1, string &str2){
+  sort(str1.begin(),str1.end());
+  sort(str2.begin(),str2.end());
+   if(str1==str2)
+       return true;
+   else
+       return false;
+}
