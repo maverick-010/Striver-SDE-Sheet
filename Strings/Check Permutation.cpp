bf6cf019ae25ea436de115f62db4bdc0134c9ef1
Mohd Umar <44342762+maverick-010@users.noreply.github.com>
2022-06-05 13:14:41 -0700

    Delete Check Permutation.cpp

--- Strings/Check Permutation.cpp
+++
@@ -1,8 +0,0 @@
-bool areAnagram(string &str1, string &str2){
-  sort(str1.begin(),str1.end());
-  sort(str2.begin(),str2.end());
-   if(str1==str2)
-       return true;
-   else
-       return false;
-}
