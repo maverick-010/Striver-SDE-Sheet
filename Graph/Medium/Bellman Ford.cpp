4bb41558e938da942ded93807e701eebe03e450f
Mohd Umar <44342762+maverick-010@users.noreply.github.com>
2022-06-16 06:44:29 -0700

    Create Bellman Ford.cpp

---
+++ Graph/Medium/Bellman Ford.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+int bellmonFord(int n, int m, int src, int dest, vector<vector<int>> &edges) {
+    vector<int>dist(n+1,1e9);
+    dist[src]=0;
+    for(int i=0;i<=n;i++){
+        for(int j=0;j<m;j++){
+            int u=edges[j][0];
+            int v=edges[j][1];
+            int wt=edges[j][2];
+            if(dist[u]!=1e9 &&(dist[u]+wt)<=dist[v] ){
+                dist[v]=dist[u]+wt;
+            }
+        }
+    }
+    bool flag=0;
+    for(int j=0;j<m;j++){
+            int u=edges[j][0];
+            int v=edges[j][1];
+            int wt=edges[j][2];
+            if(dist[u]!=1e9 &&(dist[u]+wt)<=dist[v] ){
+                dist[v]=dist[u]+wt;
+            }
+        }
+    if(flag==0){
+        return dist[dest];
+    }
+    return -1;
+}
