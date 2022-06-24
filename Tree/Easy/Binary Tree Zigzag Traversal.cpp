c4aa4ca3ab47cd0ec213c993098f6e660101becb
Mohd Umar <44342762+maverick-010@users.noreply.github.com>
2022-06-24 09:23:50 -0700

    Create Binary Tree Zigzag Traversal.cpp

---
+++ Tree/Easy/Binary Tree Zigzag Traversal.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+vector<int> zigZagTraversal(BinaryTreeNode<int> *root)
+{
+    vector<int> ans;
+    if(root == NULL) return ans;
+    queue<BinaryTreeNode<int> *> q;
+        q.push(root);
+        
+        bool leftToRight=true;
+        
+        while(!q.empty()) {
+            int size=q.size();
+            vector<int> row(size);
+            
+            for(int i=0; i<size; i++) {
+                BinaryTreeNode<int> * node=q.front();
+                q.pop();
+                
+                int index=leftToRight?i:(size-1-i);
+                
+                row[index]=node->data;
+                if(node->left != NULL) 
+                    q.push(node->left);
+                if(node->right != NULL)
+                    q.push(node->right);
+            }
+            leftToRight = !leftToRight;
+            for(int i=0; i<row.size(); i++) {
+                ans.push_back(row[i]);
+            }
+        }
+        return ans;
+}
