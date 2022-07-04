fb47b84a70d75c01f222a0bd46278a41e9c16570
Mohd Umar <44342762+maverick-010@users.noreply.github.com>
2022-07-04 03:12:22 -0700

    Create LCA of Two Nodes In A BST.cpp

---
+++ Tree/Medium/LCA of Two Nodes In A BST.cpp
@@ -0,0 +1,12 @@
+TreeNode<int>* LCAinaBST(TreeNode<int>* root, TreeNode<int>* P, TreeNode<int>* Q)
+{
+	 while(root!=NULL)
+    {
+        if(root->data>P->data && root->data>Q->data)
+            root=root->left;
+        else if(root->data<P->data && root->data<Q->data)
+            root=root->right;
+        else
+            return root;
+    }
+}
